a8101b295df0a1650d871fe5b637f7d616b3c2cc
Serhii Sakhniuk <serhii.sakhniuk@gmail.com>
2018-08-09 14:56:18 +0300

    Add some tests

--- test/integration.test.js
+++ test/integration.test.js
@@ -12,13 +12,68 @@ describe('API tests', () => {
       .end((err, response) => {
         if (err) {
           done(err);
-        }
-        else {
+        } else {
           expect(response.status).to.equal(200);
           expect(response.type).to.equal('application/json');
           done();
         }
       });
   });
+
+  it('should be create new user', (done) => {
+    supertest(server)
+      .get('/sectors')
+      .end((err, response) => {
+        if (err) {
+          done(err);
+        } else {
+          expect(response.status).to.equal(200);
+          expect(response.type).to.equal('application/json');
+          expect(response.ok).to.equal(true);
+          done();
+        }
+      });
+  });
+
+  it('handle routing errors', (done) => {
+    supertest(server)
+      .get('/wrongroute')
+      .end((err, response) => {
+        if (err) {
+          done(err);
+        } else {
+          expect(response.status).equals(404);
+          const errorType = 'ResourceNotFound';
+          expect( response.body['code'] ).equals( errorType );
+          done();
+        }
+      });
+  });
+
+  it('has API version 1', (done) => {
+    supertest(server)
+      .get( '/sectors' )
+      .set( 'accept-version', '~1' )
+      .end((err, response) => {
+        if (err) {
+          done(err);
+        }
+        else {
+          expect(response.status).to.equal(200);
+          done();
+        }
+      });
+  });
+
+  it('Should throw an error', done => {
+    supertest(server)
+      .post('/save-selectors')
+      .end((err, response) => {
+        expect(response.status).to.equal(500);
+        done();
+      })
+
+  })
+
 })
 
