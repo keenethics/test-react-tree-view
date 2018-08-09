78a528577a9d37fd5ad79b5136d9ed724bf7f8f7
Serhii Sakhniuk <serhii.sakhniuk@gmail.com>
2018-08-09 12:37:55 +0300

    Add

--- server/index.js
+++ server/index.js
@@ -29,6 +29,7 @@ server.get('/sectors', async (request, response, next) => {
   const user = await getUserFromJwt(token)
   if (user) {
     // existing user
+    logger.info(`User with id ${user.userId} is trying to get sectors`)
     const { selectedSectors } = user
     responseData.selectedSectors = selectedSectors
     response.send(responseData)
